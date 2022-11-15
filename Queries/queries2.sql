635aae459c79527347adec41f81f2f0430422c21
DLY79 <lyndsdavid@gmail.com>
2022-11-15 11:09:03 -0400

    update index

--- Queries/queries2.sql
+++ Queries/queries2.sql
@@ -34,13 +34,14 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-DROP TABLE titles CASCADE;
+DROP TABLE titles;
 CREATE TABLE titles (
     emp_no INT NOT NULL,
     title VARCHAR NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no, title, from_date)
     );
 
 CREATE TABLE dept_emp (
@@ -204,7 +205,7 @@ WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
     
 SELECT * FROM emp_info
     
--- List of managers per department
+-- List of managers per department 7.3.5
 SELECT  dm.dept_no,
         d.dept_name,
         dm.emp_no,
